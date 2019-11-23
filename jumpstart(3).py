14d7890a1bc60f5b6f9b69a1c0ab55991cb9420c
Alexander Nazarovsky <ANazarovsky@gmail.com>
2019-11-23 14:24:24 +0300

    Birthday countdown

---
+++ jumpstart(3).py
@@ -0,0 +1,32 @@
+import datetime
+
+
+def birthday():
+    year = int(input('What year were you born [YYYY]? '))
+    month = int(input('What month were you born [MM]? '))
+    date = int(input('What date were you born [DD]? '))
+    birthday = datetime.date(year, month, date)
+    return birthday
+
+
+def calculation(date):
+    today = datetime.date.today()
+    current_year = datetime.date(today.year, date.month, date.day)
+    return (current_year - today).days
+
+
+def output(days):
+    if days < 0:
+        print(f'You had your birthday {-days} days ago this year.')
+    elif days > 0:
+        print(f'Your birthday is in {days} days!')
+    else:
+        print("HAPPY BIRTHDAY!!!")
+
+
+def main():
+    output(calculation(birthday()))
+
+
+if __name__ == '__main__':
+    main()
