17ada1fdf5c8b506ab0c74e1ba80dceb784c3e56
Alexander Nazarovsky <ANazarovsky@gmail.com>
2019-12-18 13:39:05 +0300

    Birthday countdown

--- jumpstart_3.py
+++ jumpstart_3.py
@@ -2,9 +2,9 @@ import datetime
 
 
 def birthday():
-    year = int(input('What year were you born [YYYY]? '))
-    month = int(input('What month were you born [MM]? '))
-    date = int(input('What date were you born [DD]? '))
+    year = int(input("What year were you born [YYYY]? "))
+    month = int(input("What month were you born [MM]? "))
+    date = int(input("What date were you born [DD]? "))
     birthday = datetime.date(year, month, date)
     return birthday
 
@@ -17,9 +17,9 @@ def calculation(date):
 
 def output(days):
     if days < 0:
-        print(f'You had your birthday {-days} days ago this year.')
+        print(f"You had your birthday {-days} days ago this year.")
     elif days > 0:
-        print(f'Your birthday is in {days} days!')
+        print(f"Your birthday is in {days} days!")
     else:
         print("HAPPY BIRTHDAY!!!")
 
