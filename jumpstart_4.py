a60b620fff877d3126f4038751493c81ba9d6b07
Alexander Nazarovsky <ANazarovsky@gmail.com>
2019-12-19 13:17:53 +0300

    Journal app

---
+++ jumpstart_4.py
@@ -0,0 +1,36 @@
+import os
+from journal import Journal
+import time
+
+
+def header():
+    print('> Welcome to your LOG journal <\n')
+
+
+def event_loop(journal):
+    while True:
+        cmd = input("- [L]ist entries\n- [A]dd entry\n- [S]ave journal\n- [U]pload journal\n- e[X]it\n").upper().strip()
+        if cmd == "L":
+            journal.log()
+        elif cmd == "A":
+            journal.add_entry(input(">>"))
+        elif cmd == "S":
+            journal.save()
+            print("Journal saved")
+        elif cmd == "U":
+            journal.load()
+        elif cmd == "X":
+            print("Goodbye")
+            break
+        else:
+            print(f"{cmd} is undefined command")
+
+            
+def main():
+    header()
+    journal = Journal()
+    event_loop(journal)
+
+    
+if __name__ == "__main__":
+    main()
