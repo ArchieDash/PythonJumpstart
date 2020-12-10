ad6eb5eee8644644330604c431bae912bcfa651d
Alexander Nazarovsky <ANazarovsky@gmail.com>
2020-12-11 01:18:06 +0300

    Classes for Wizard game

--- actors.py
+++ actors.py
@@ -1,7 +1,23 @@
+import random
+
+
 class Wizard:
     def __init__(self, name, level):
         self.name = name
         self.level = level
+
+    def attack(self, creature):
+        print(f"A wizard {self.name} attacks {creature.name}!")
+        my_roll = random.randint(1, 12) * self.level
+        creature_roll = random.randint(1, 12) * creature.level
+        print(f"You roll {my_roll}...")
+        print(f"{creature.name} roll {creature_roll}...")
+        if my_roll >= creature_roll:
+            print(f"The wizard has handily triumphed over {creature.name}.")
+            return True
+        else:
+            print("The wizard has been DEFEATED!!!")
+            return False
 
 
 class Creature:
