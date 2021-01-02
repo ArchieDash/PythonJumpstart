9bfe273563c17022987c7ec3b662d23bee1a69a5
Alexander Nazarovsky <ANazarovsky@gmail.com>
2021-01-03 01:35:24 +0300

    Updated weather forecast service

--- weather_updated.py
+++ weather_updated.py
@@ -1,14 +1,17 @@
 import pyfiglet
 import collections
+import requests
 
 
 Location = collections.namedtuple("Location", "city state country")
+Weather = collections.namedtuple("Weather", "location units temp condition")
 
 
 def main():
     print(pyfiglet.figlet_format("Weather App"))
     location_text = input("Where do you want the weather report (e.g. Portland, US)? ")
-    city, state, country = convert_plain_text(location_text)
+    location = convert_plain_text(location_text)
+    report_weather(location)
     
 
 def convert_plain_text(location_text):
@@ -18,7 +21,7 @@ def convert_plain_text(location_text):
     parts = location_text.split(",")
     city = ""
     state = ""
-    country = "usa"
+    country = "us"
     if len(parts) == 1:
         city = parts[0].strip()
     elif len(parts) == 2:
@@ -31,6 +34,48 @@ def convert_plain_text(location_text):
     else:
         return None
     return Location(city, state, country)
+
+
+def call_weather_api(loc):
+    url = "https://weather.talkpython.fm/api/weather"
+    params = {
+        "city": loc.city,
+        "state": loc.state,
+        "country": loc.country
+    }
+    resp = requests.get(url, params=params)
+    if resp.status_code in {400, 404, 500}:
+        print(f"Error: {resp.text}.")
+    data = resp.json()
+    return convert_api_to_weather(data, loc)
+
+
+def convert_api_to_weather(data, loc):
+    temp = data.get("forecast").get("temp")
+    w = data.get("weather")
+    condition = f"{w.get('category')}: {w.get('description').capitalize()}."
+    weather = Weather(loc, data.get("units"), temp, condition)
+    return weather
+
+
+def get_location_name(location):
+    if not location.state:
+        return f"{location.city.capitalize()}, {location.country.upper()}"
+    else:
+        return f"{location.city.capitalize()}, {location.state.upper()} {location.country.upper()}"
+
+
+def get_scale(weather):
+    if weather.units == "imperial":
+        return "F"
+    else:
+        return "C"
+
+
+def report_weather(location):
+    weather = call_weather_api(location)
+    scale = get_scale(weather)
+    print(f"The weather in {get_location_name(location)} is {weather.temp} {scale} and {weather.condition}")
 
 
 if __name__ == "__main__":
